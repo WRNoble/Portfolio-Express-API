781491b77cff2b3b5195ddef144e57d45f19e886
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-07-20 10:56:45 -0400

    node works

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const sendGrid = require("@sendGrid/mail");
+const { response, request } = require("express");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use((request, response, next) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  response.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-type, Authorization"
+  );
+  next();
+});
+
+app.get("/api", (request, response) => {
+  response.send("API is running");
+});
+
+app.post("/api/email", (request, response, next) => {
+  sendGrid.setApiKey(
+    "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"
+  );
+  const message = {
+    to: "wrnoblejrsbts@gmail.com",
+    from: request.body.email,
+    subject: "Portfolio Contact",
+    text: request.body.message,
+  };
+  sendGrid
+    .send(message)
+    .then((result) => {
+      response.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      console.log("error", err);
+      response.status(401).json({
+        success: false,
+      });
+    });
+});
+
+app.listen(3050, "0.0.0.0");
